248b1c9ed66af95fba6a436a8fa218bbeb7b5e2a
Ashley Contenta <ashleycontenta@Matts-MacBook-Pro.local>
2021-03-26 12:00:22 -0700

    faq page added

--- routes/index.js
+++ routes/index.js
@@ -10,4 +10,49 @@ router.get("/", function (req, res, next) {
   });
 });
 
+router.get("/adopt", function (req, res, next) {
+  res.render("adopt", {
+    layout: "wrapper",
+    template: "adopt",
+    title: "Adopt",
+    isAdopt: true,
+  });
+});
+
+router.get("/faq", function (req, res, next) {
+  res.render("faq", {
+    layout: "wrapper",
+    template: "faq",
+    title: "FAQ",
+  });
+});
+
+  // res.render("/puppies", {
+  //   layout: "wrapper",
+  //   template: "puppies",
+  //   title: "Puppies",
+  //   isPuppies: true,
+  // });
+
+  // res.render("/parents", {
+  //   layout: "wrapper",
+  //   template: "parents",
+  //   title: "Parents",
+  //   isParents: true,
+  // });
+
+  // res.render("/info", {
+  //   layout: "wrapper",
+  //   template: "info",
+  //   title: "Info",
+  //   isInfo: true,
+  // });
+
+  // res.render("/contact", {
+  //   layout: "wrapper",
+  //   template: "contact",
+  //   title: "Contact",
+  //   isContact: true,
+  // });
+
 module.exports = router;
