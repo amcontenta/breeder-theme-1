326892cbccacff22ffaa683084506af0dd722056
amcontenta <amcontenta@gmail.com>
2021-04-18 11:21:05 -0700

    scroll reveals

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,5 +1,4 @@
 $(document).ready(function () {
-  console.log('doc has loaded');
   $(".js-hamburger-menu").on("click", function () {
     $(".js-mobile-nav-overlay").addClass("show");
     $("body").addClass("overflow-hidden");
@@ -10,3 +9,27 @@ $(document).ready(function () {
     $("body").removeClass("overflow-hidden");
   });
 });
+
+ScrollReveal().reveal('.fade-in, h1', {
+  duration: 1500,
+});
+
+ScrollReveal().reveal('.fade-up', {
+  distance: '50px',
+  duration: 1500,
+});
+
+ScrollReveal().reveal('.fade-up-in', {
+  distance: '50px',
+  duration: 1500,
+});
+
+
+
+
+
+
+// const ash = {
+//   name: "ashley",
+//   age: 32,
+// };
